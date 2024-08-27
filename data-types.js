fa5f9fc7178d64a04d2669f344e9489d7069155b
nagaraj <nagaraj.m.karakikatti6912@gmail.com>
2024-08-27 21:05:03 +0530

    data types done

--- data-types.js
+++ data-types.js
@@ -1,1 +1,38 @@
 console.log("hello")
+//data types
+//premitive def:-(predefined DataTypes)
+
+//String => to store a sequence of characters
+//Boolean => to store a boolean value true/false
+//Number => to store a int/float values
+//null => it stores nothing and its type is an object
+//Undefined => value is not defined 
+//Symbol => it contais unique id
+//BigInt => used to store a big integer value
+const string="hello"
+const bool=true
+const num=12
+const nul=null
+const undefine=undefined
+const id=Symbol("hello")
+const Bint=BigInt(100000000000000000000000000000008)
+// console.log(`${string}\n${bool}\n${num}\n${nul}\n${undefine}\n${Symbol}\n${Bint}`)
+
+//non-premitive def:-User Defined or Reference Type
+
+//Array => used to store set of different or same type of data
+//Obect => used to create a key value pairs and methods
+//Functions => used to run a set of work any number of times we can return also
+
+const arr=["string1",2,true]
+
+const obj = {
+    name:"xyz",
+    age:20,
+    male:true,
+}
+
+function fun(a,b) {
+    c=a+b
+    return c;
+}
