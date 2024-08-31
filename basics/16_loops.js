0dc3661d69afba1b5601e66e13beedaf9197f314
nagaraj <nagaraj.m.karakikatti6912@gmail.com>
2024-08-31 20:07:14 +0530

    controll flow and loops

---
+++ basics/16_loops.js
@@ -0,0 +1,34 @@
+//for and while loop
+
+const name="hello"
+//for loop
+// for (let i = 0; i < name.length; i++) {
+//     const element = name[i]
+//     console.log(element)
+// }
+
+//while and do while
+
+let i=0
+// while (i < name.length) {
+//     console.log(name[i])
+//     i+=1
+// }
+
+// do {
+//     console.log(name[i]);
+//     i++
+// } while (i<name.length);
+
+//for of loop and continue amd break;
+
+    // const greeting=[1,2,3,4,5,6]
+    // for (const i of greeting) {
+    //     if(i==3){
+    //         continue
+    //     }
+    //     if(i==6){
+    //         break
+    //     }
+    //     console.log(i)
+    // }
