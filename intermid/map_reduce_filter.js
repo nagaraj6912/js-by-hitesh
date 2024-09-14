9631f0bc8b9c3cbc89c36ead52abcefc357a24d2
nagaraj <nagaraj.m.karakikatti6912@gmail.com>
2024-09-14 09:03:36 +0530

    map reduce filter

--- intermid/map_reduce_filter.js
+++ intermid/map_reduce_filter.js
@@ -33,10 +33,66 @@ const max = arr.reduce((acc, cur) => {
     //  console.log(acc) //accumulator contains the return value
     return acc
 })
-// console.log(max)
+console.log(max)
 const user = [
     {firstname: "akshay",lastname: "saini",age: 2},
     {firstname: "donald",lastname: "trump",age: 75},
     {firstname: "elon",lastname: "musk",age: 50},
     {firstname: "deepika",lastname: "padukone",age: 26},
 ];
+
+//find the name of people with same age
+//find the number of people and their name whose age is 50 or above 50
+
+const user = [{
+    firstname: "akshay",
+    lastname: "saini",
+    age: 26
+},
+{
+    firstname: "donald",
+    lastname: "trump",
+    age: 75
+},
+{
+    firstname: "elon",
+    lastname: "musk",
+    age: 50
+},
+{
+    firstname: "deepika",
+    lastname: "padukone",
+    age: 26
+},
+];
+
+const output = user.reduce(function (acc, cur) {
+    // console.log(cur.age)
+    if (acc[cur.age]) {
+        let newage = cur.age
+        // console.log(cur.firstname)
+        acc[cur.age] = ++acc[cur.age];
+        const names = user.filter((j) => {
+            if (j.age == newage) {
+                // console.log(j.firstname, " ", j.age)
+            }
+        })
+    } else {
+        acc[cur.age] = 1
+    }
+    return acc
+}, {},);
+// console.log(output)
+
+
+const name=user.filter(function(i){
+    let dets
+
+    if(i.age>=50){
+        dets=i.age+i.firstname+i.lastname
+    }
+    // console.log(dets)
+    return dets;
+}).map((j)=>{
+    // console.log(j.firstname+" "+j.lastname+" "+j.age)
+})
